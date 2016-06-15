b55aea7d81875b79a4679841d41b01cb26de3960
Nur Bar <nbfbce@ymail.com>
2016-06-15 17:20:58 +0300

    WIP

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ var sass = require('gulp-sass');
 var notify = require("gulp-notify");
 var sourcemaps = require('gulp-sourcemaps');
 var hogan = require('gulp-hogan');
+var minifyjs = require('gulp-minify');
+var concat = require('gulp-concat');
 
 gulp.task('sass', function () {
     return gulp.src('assets/stylesheets/bootstrap.scss')
@@ -13,6 +15,14 @@ gulp.task('sass', function () {
         .pipe(notify('SASS compiled'));
 });
 
+gulp.task('js',function(){
+    return gulp.src('assets/javascripts/extras/*.js')
+        .pipe(minifyjs({noSource : true}))
+        .pipe(concat('main.js',{newLine: ''}))
+        .pipe(gulp.dest("assets/javascripts"))
+        .pipe(notify('Js Updated'));
+});
+
 gulp.task('hogan', function () {
     return gulp.src('assets/templates/*.html')
         .pipe(hogan({}, null, '.html'))
@@ -20,8 +30,9 @@ gulp.task('hogan', function () {
         .pipe(notify('Hogan compiled'));
 });
 
-gulp.task('default',['sass','hogan'],function(){
+gulp.task('default',['sass','hogan','js'],function(){
 
     gulp.watch("assets/stylesheets/extras/*.scss",['sass']);
+    gulp.watch("assets/javascripts/extras/*.js",['js']);
     gulp.watch("assets/templates/**/*.html",['hogan']);
 });
