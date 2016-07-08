8a5071fd7f8cb78fec48916c4f3e5b4c075570c0
Cucurrucho <adam.bar91@gmail.com>
2016-07-08 13:12:57 -0300

    Heading class, with js to do the profilepic and hover, profile pic is in a js var and given to where it is needed through a function (i think its better, makes it easier to change), also with breadcrumbs, i have a nice script to give them the active class, but we need to decide how to do the dropdown or if we do direct links

--- assets/javascripts/data/heading/profile-img-src.js
+++ assets/javascripts/data/heading/profile-img-src.js
@@ -1,5 +1,2 @@
 var profilePic = "../assets/stylesheets/extras/images/profile_pic.jpg";
-var elems = document.getElementsByClassName("profile-img");
-for (var i = 0; i < elems.length; i+= 1) {
-    elems[i].src = profilePic;
-}
+$('.profile-img').attr("src", profilePic);
