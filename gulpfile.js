463851fc0d5ebb20f636194fff8f210318e64709
Nur Bar <nbfbce@ymail.com>
2016-07-21 17:43:46 -0300

    A lot of changes to gulp happened.

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,14 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var notify = require("gulp-notify");
 var sourcemaps = require('gulp-sourcemaps');
-var hogan = require('gulp-hogan');
+var data = require('gulp-data');
+var fs = require('fs');
+var path = require('path');
+var mustache = require('gulp-mustache');
 var minifyjs = require('gulp-minify');
 var concat = require('gulp-concat');
 var autoprefixer = require('gulp-autoprefixer');
+var plumber = require('gulp-plumber');
 var browserSync = require('browser-sync').create();
 
 gulp.task('browser-sync', function () {
@@ -14,11 +18,9 @@ gulp.task('browser-sync', function () {
 	});
 });
 
-gulp.task('js-watch', ['js'], browserSync.reload);
-gulp.task('hogan-watch', ['hogan'], browserSync.reload);
-
 gulp.task('sass', function () {
 	return gulp.src('assets/stylesheets/bootstrap.scss')
+		.pipe(plumber())
 		.pipe(sourcemaps.init())
 		.pipe(sass().on('error', sass.logError))
 		/*        .pipe(autoprefixer({
@@ -28,11 +30,12 @@ gulp.task('sass', function () {
 		.pipe(sourcemaps.write('maps'))
 		.pipe(gulp.dest('assets/css/'))
 		.pipe(notify({message: 'SASS compiled', onLast: true}))
-		.pipe(browserSync.stream());
+		.pipe(browserSync.reload({stream: true}));
 });
 
 gulp.task('js', function () {
 	return gulp.src('assets/javascripts/extras/*.js')
+		.pipe(plumber())
 		.pipe(minifyjs({noSource: true}))
 		.pipe(concat('main.js', {newLine: ''}))
 		.pipe(gulp.dest("assets/javascripts"))
@@ -41,17 +44,21 @@ gulp.task('js', function () {
 });
 
 
-gulp.task('hogan', function () {
+gulp.task('mustache', function () {
 	return gulp.src('assets/templates/*.html')
-		.pipe(hogan({}, null, '.html'))
+		.pipe(plumber())
+		.pipe(data(function(file) {
+			return JSON.parse(fs.readFileSync('assets/templates/views/' + path.basename(file.path,".html") + '.json'));
+		}))
+		.pipe(mustache())
 		.pipe(gulp.dest('public'))
 		.pipe(notify({message: 'Hogan compiled', onLast: true}))
 		.pipe(browserSync.reload({stream: true}));
 });
 
-gulp.task('default', ['sass', 'hogan', 'js', 'browser-sync'], function () {
+gulp.task('default', ['sass', 'mustache', 'js', 'browser-sync'], function () {
 
 	gulp.watch("assets/stylesheets/extras/*.scss", ['sass']);
 	gulp.watch("assets/javascripts/extras/*.js", ['js']);
-	gulp.watch("assets/templates/**/*.html", ['hogan']);
+	gulp.watch(["assets/templates/**/*.html","assets/templates/**/*.mustache","assets/templates/**/*.json"], ['mustache']);
 });
